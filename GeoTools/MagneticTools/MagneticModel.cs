bbfb3a98fbb8275b8fd29ee7254fe968d76d92bc
Prithvi Shivaraman <prithvisagar.shivaraman@gmail.com>
2021-09-15 15:02:04 -0400

    Finished magnetic model

--- GeoTools/MagneticTools/MagneticModel.cs
+++ GeoTools/MagneticTools/MagneticModel.cs
@@ -16,5 +16,48 @@ namespace AviationCalcUtilNet.GeoTools.MagneticTools
         [DllImport("aviationcalc")] private static extern double MagModelGetModelEpoch(IntPtr model);
         [DllImport("aviationcalc")] private static extern IntPtr MagModelGetModelName(IntPtr model);
         [DllImport("aviationcalc")] private static extern InteropDateStruct MagModelGetReleaseDate(IntPtr model);
+
+        internal MagneticModel(IntPtr ptr)
+        {
+            this.ptr = ptr;
+        }
+
+        public MagneticModel(double modelEpoch, string modelName, DateTime releaseDate, List<MagneticModelCoefficients> coefficients)
+        {
+            IntPtr[] coeffsArr = new IntPtr[coefficients.Count];
+
+            for (int i = 0; i < coefficients.Count; i++)
+            {
+                coeffsArr[i] = coefficients[i].ptr;
+            }
+
+            ptr = CreateMagModel(modelEpoch, modelName, InteropUtil.ManagedDateToDateStruct(releaseDate), coeffsArr, coeffsArr.Length);
+        }
+
+        public MagneticModelCoefficients GetCoeffs(int n, int m)
+        {
+            var coeffs = MagModelGetCoeffs(ptr, n, m);
+            if (coeffs == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            return new MagneticModelCoefficients(coeffs);
+        }
+
+        public double ModelEpoch => MagModelGetModelEpoch(ptr);
+
+        public string ModelName {
+            get {
+                return Marshal.PtrToStringAnsi(MagModelGetModelName(ptr));
+            }
+        }
+
+        public DateTime ReleaseDate => InteropUtil.DateStructToManagedDate(MagModelGetReleaseDate(ptr));
+
+        ~MagneticModel()
+        {
+            DisposeMagModel(ptr);
+        }
     }
 }
